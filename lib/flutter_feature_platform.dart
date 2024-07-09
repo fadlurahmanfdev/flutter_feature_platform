b9c348de9ff29859037a9f19ddafb7f4197f2061
fadlurahmanf <tffajari@gmail.com>
2024-07-09 15:54:36 +0700

    add export

--- lib/flutter_feature_platform.dart
+++ lib/flutter_feature_platform.dart
@@ -3,3 +3,6 @@ library flutter_feature_platform;
 export 'package:device_info_plus/device_info_plus.dart';
 export 'package:package_info_plus/package_info_plus.dart';
 export 'package:platform_device_id_v3/platform_device_id.dart';
+
+export 'src/data/repository/feature_platform_repository.dart';
+export 'src/data/repository/feature_platform_repository_impl.dart';
